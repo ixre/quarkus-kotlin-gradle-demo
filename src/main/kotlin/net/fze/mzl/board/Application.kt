a4e08c654d190d3bc6d73233ed48d2922f07d029
jarrysix <jarrysix@gmail.com>
2020-05-01 22:14:30 +0800

    Fix: RESTEASY003210: Could not find resource for full path

--- src/main/kotlin/net/fze/mzl/board/Application.kt
+++ src/main/kotlin/net/fze/mzl/board/Application.kt
@@ -4,11 +4,12 @@ import io.quarkus.runtime.StartupEvent
 import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.event.Observes
 import javax.ws.rs.ApplicationPath
+import javax.ws.rs.core.Application
 
 
 @ApplicationScoped
 @ApplicationPath("/")
-class Application {
+class Application: Application() {
     fun onStart(@Observes event: StartupEvent?) {
         println("app started..")
     }

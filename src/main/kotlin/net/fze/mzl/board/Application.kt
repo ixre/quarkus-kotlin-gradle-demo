1a0f4a855aed3bec83469fa7dab93235872c7185
jarrysix <jarrysix@gmail.com>
2020-05-01 23:42:11 +0800

    Application.kt

--- src/main/kotlin/net/fze/mzl/board/Application.kt
+++ src/main/kotlin/net/fze/mzl/board/Application.kt
@@ -3,14 +3,24 @@ package net.fze.mzl.board
 import io.quarkus.runtime.StartupEvent
 import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.event.Observes
-import javax.ws.rs.ApplicationPath
-import javax.ws.rs.core.Application
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.core.Response
 
 
 @ApplicationScoped
-@ApplicationPath("/")
-class Application: Application() {
+class Application {
     fun onStart(@Observes event: StartupEvent?) {
         println("app started..")
+    }
+}
+
+@Path("/")
+class HomeResource {
+    @GET
+    fun home(): Response {
+        return Response.status(302)
+                .header("Location", "/app/index.html")
+                .build()
     }
 }
